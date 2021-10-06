fcc68dfd60efd3de272c8c53d32e293daf882d04
Fedor1533 <fgubanov1533@gmail.com>
2021-10-06 11:10:44 +0300

    Add requirements.txt file

--- mylabelsystem/settings.py
+++ mylabelsystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6**y5588(q@y#+^h54z$-j^8h%=kv$)4y!&56=u&+mk%wo#1ja'
+# SECRET_KEY = 'django-insecure-6**y5588(q@y#+^h54z$-j^8h%=kv$)4y!&56=u&+mk%wo#1ja'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
