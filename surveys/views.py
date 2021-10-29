c33edb0ec0d6d1043f10d589a0c671f63621a0a3
Fedor1533 <fgubanov1533@gmail.com>
2021-10-29 19:14:32 +0300

    Make comments - comments logic, list of comments and superuser comment.

--- surveys/views.py
+++ surveys/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from .forms import NewCommentForm
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 from .models import *
 
 def home(request):
@@ -12,14 +14,19 @@ def home(request):
     fields.remove('comments')  # to not show foreignkey - comments on home page
     return render(request, 'home.html', {'surveys': surveys, 'fields': fields})
 
+@login_required
 def source(request, pk):
     surveys = get_list_or_404(Survey)
     source_f = get_object_or_404(Source, pk=pk)  # get object-source chosen by user on main page
     dup_sources = Source.objects.filter(dup_id=source_f.dup_id)  # get all object-sources related to this source
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         source_id = request.POST.get('source_id', None)  # get from hidden field source pk for current tab
         source = dup_sources.get(pk=source_id)  # get current object-source
+        try:
+            comment = Comment.objects.get(source=source, created_by=request.user)
+            comment.updated_at = datetime.now()
+        except Comment.DoesNotExist:
+            comment = Comment.objects.create(comment='test', source=source, created_by=request.user)
 
         # Make master source-object or make normal
         if request.POST.get('master', False):
@@ -32,24 +39,22 @@ def source(request, pk):
             source.master_source = False if source.master_source else True
             source.save()
 
-            return redirect('source', pk=source_id)  # TODO: redirect to the created topic page
+            return redirect('source', pk=source_id)
 
         # Create/Edit comment
         else:
-            form = NewCommentForm(request.POST)  # use existing or created comment
+            form = NewCommentForm(request.POST, instance=comment)  # use existing or created comment
 
             if form.is_valid():
-                comment = form.save(commit=False)
-                comment.source = source
-                comment.created_by = user
+                form.save()
                 comment.save()
 
-                if source.master_source:
+                if request.user.is_superuser:
                     source.comment = form.cleaned_data.get('comment')
                     source.source_class = comment.source_class
                     source.save()
 
-            return redirect('source', pk=source_id)  # TODO: redirect to the created topic page
+            return redirect('source', pk=source_id)
     else:
         form = NewCommentForm()
     return render(request, 'source.html', {'surveys': surveys, 'source_f': source_f, 'dup_sources': dup_sources, 'form': form})
