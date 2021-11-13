2c5ecc8bcfe535882b550d79a4a8a3b75e624d9f
Fedor1533 <fgubanov1533@gmail.com>
2021-11-13 17:52:22 +0300

    Improve comments - add follow up and class, add optical source and comment class to models.

--- surveys/models.py
+++ surveys/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import Truncator
 from django.contrib.auth.models import User
 
+
 class Survey(models.Model):
     name = models.PositiveIntegerField(unique=True)
     started_at = models.DateTimeField(auto_now_add=True)
@@ -14,9 +15,10 @@ class Survey(models.Model):
     def get_sources_count(self):
         return Source.objects.filter(survey=self).count()
 
+
 class Source(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    #  for sources and their measurements in surveys
+
     RA = models.DecimalField(max_digits=9, decimal_places=5)
     DEC = models.DecimalField(max_digits=9, decimal_places=5)
 
@@ -39,17 +41,17 @@ class Source(models.Model):
     dup_id = models.PositiveIntegerField(blank=True, null=True)
 
     # TODO: Think about formats and view of NEW fields
-    L = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    B = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    R98 = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    g_d2d = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    g_s = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    g_nsrc = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    g_gmag = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    s_d2d = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    s_id = models.CharField(max_length=100, blank=True, null=True)
-    s_z = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
-    s_otype = models.CharField(max_length=100, blank=True, null=True)
+    L = models.FloatField(blank=True, null=True),
+    B = models.FloatField(blank=True, null=True),
+    R98 = models.FloatField(blank=True, null=True),
+    g_d2d = models.FloatField(blank=True, null=True),
+    g_s = models.FloatField(blank=True, null=True),
+    g_nsrc = models.FloatField(blank=True, null=True),
+    g_gmag = models.FloatField(blank=True, null=True),
+    s_d2d = models.FloatField(blank=True, null=True),
+    s_id = models.FloatField(blank=True, null=True),
+    s_z = models.FloatField(blank=True, null=True),
+    s_otype = models.FloatField(blank=True, null=True),
     # w1,2,3
     w_e1 = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
     w_e2 = models.DecimalField(max_digits=9, decimal_places=5, blank=True, null=True)
@@ -129,6 +131,7 @@ class Source(models.Model):
                 value = getattr(self, field.name, None)
                 yield (field.name, value)
 
+
 class Comment(models.Model):
     comment = models.TextField(max_length=2000)
     follow_up = models.TextField(max_length=1000, blank=True, null=True)
@@ -151,4 +154,121 @@ class Comment(models.Model):
         return 'Comment: {}'.format(truncated_comment.chars(10))
 
 
+class OptSource(models.Model):
+    name = models.CharField(max_length=150)
 
+    RA = models.DecimalField(max_digits=9, decimal_places=5)
+    DEC = models.DecimalField(max_digits=9, decimal_places=5)
+
+    ztf_name = models.CharField(max_length=100, blank=True, null=True)
+
+    source_class = models.CharField(
+        max_length=20,
+        choices=Source.CLASS_CHOICES,
+        default='NaN',
+        blank=True, null=True,
+    )
+    # Status
+    master_source = models.BooleanField(default=False, blank=True, null=True)
+    probable_source = models.BooleanField(default=False, blank=True, null=True)
+
+    # Legacy Surveys attributes
+    ls_type = models.CharField(max_length=100, blank=True, null=True),
+    ls_ra = models.FloatField(blank=True, null=True),
+    ls_dec = models.FloatField(blank=True, null=True),
+    ls_bx = models.FloatField(blank=True, null=True),
+    ls_by = models.FloatField(blank=True, null=True),
+    ls_ebv = models.FloatField(blank=True, null=True),
+    ls_mjd_min = models.FloatField(blank=True, null=True),
+    ls_mjd_max = models.FloatField(blank=True, null=True),
+    ls_ref_cat = models.CharField(max_length=100, blank=True, null=True),
+    ls_ref_id = models.FloatField(blank=True, null=True),
+    ls_pmra = models.FloatField(blank=True, null=True),
+    ls_pmdec = models.FloatField(blank=True, null=True),
+    ls_parallax = models.FloatField(blank=True, null=True),
+    ls_ref_epoch = models.FloatField(blank=True, null=True),
+
+    # SDSS attributes
+    sdss_MODE = models.FloatField(blank=True, null=True),
+    sdss_CLEAN = models.FloatField(blank=True, null=True),
+    sdss_ra = models.FloatField(blank=True, null=True),
+    sdss_dec = models.FloatField(blank=True, null=True),
+    sdss_RAERR = models.FloatField(blank=True, null=True),
+    sdss_DECERR = models.FloatField(blank=True, null=True),
+    sdss_cModelFlux_u = models.FloatField(blank=True, null=True),
+    sdss_cModelFluxIvar_u = models.FloatField(blank=True, null=True),
+    sdss_cModelFlux_g = models.FloatField(blank=True, null=True),
+    sdss_cModelFluxIvar_g = models.FloatField(blank=True, null=True),
+
+    # Pan-STARRS1 DR2 attributes
+    ps_raBest = models.FloatField(blank=True, null=True),
+    ps_decBest = models.FloatField(blank=True, null=True),
+    ps_raStack = models.FloatField(blank=True, null=True),
+    ps_decStack = models.FloatField(blank=True, null=True),
+    ps_raStackErr = models.FloatField(blank=True, null=True),
+    ps_decStackErr = models.FloatField(blank=True, null=True),
+    ps_raMean = models.FloatField(blank=True, null=True),
+    ps_decMean = models.FloatField(blank=True, null=True),
+    ps_raMeanErr = models.FloatField(blank=True, null=True),
+    ps_decMeanErr = models.FloatField(blank=True, null=True),
+    ps_objInfoFlag = models.FloatField(blank=True, null=True),
+    ps_qualityFlag = models.FloatField(blank=True, null=True),
+    ps_primaryDetection = models.FloatField(blank=True, null=True),
+    ps_bestDetection = models.FloatField(blank=True, null=True),
+    ps_duplicat = models.CharField(max_length=100, blank=True, null=True),
+    ps_d_to = models.FloatField(blank=True, null=True),
+    ps_fitext = models.CharField(max_length=100, blank=True, null=True),
+    ps_devaucou = models.CharField(max_length=100, blank=True, null=True),
+    ps_star = models.CharField(max_length=100, blank=True, null=True),
+
+    # GAIA attributes
+    gaiaedr3_ra = models.FloatField(blank=True, null=True),
+    gaiaedr3_ra_error = models.FloatField(blank=True, null=True),
+    gaiaedr3_dec = models.FloatField(blank=True, null=True),
+    gaiaedr3_dec_error = models.FloatField(blank=True, null=True),
+    gaiaedr3_parallax = models.FloatField(blank=True, null=True),
+    gaiaedr3_parallax_error = models.FloatField(blank=True, null=True),
+    gaiaedr3_pm = models.FloatField(blank=True, null=True),
+    gaiaedr3_pmra = models.FloatField(blank=True, null=True),
+    gaiaedr3_pmra_error = models.FloatField(blank=True, null=True),
+    gaiaedr3_pmdec = models.FloatField(blank=True, null=True),
+    gaiaedr3_pmdec_error = models.FloatField(blank=True, null=True),
+
+    # WISE attributes
+    ls_flux_w1 = models.FloatField(blank=True, null=True),
+    ls_flux_w2 = models.FloatField(blank=True, null=True),
+    ls_flux_w3 = models.FloatField(blank=True, null=True),
+    ls_flux_w4 = models.FloatField(blank=True, null=True),
+
+    def __str__(self):
+        return 'OptSource: {}'.format(self.name)
+
+    def get_comment_count(self):
+        return Comment.objects.filter(source=self).count()
+
+    def get_last_comment(self):
+        return Comment.objects.filter(source=self).order_by('-created_at').first()
+
+    def __iter__(self):
+        f_list = []  # TODO: how to not show fields smarter
+        for field in self._meta.get_fields():
+            if field.name not in f_list:
+                value = getattr(self, field.name, None)
+                yield (field.name, value)
+
+
+class OptComment(models.Model):
+    comment = models.TextField(max_length=1500)
+    follow_up = models.TextField(max_length=500, blank=True, null=True)
+
+    # TODO: Status field
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='opt_comments')
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    opt_source = models.ForeignKey(OptSource, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        truncated_comment = Truncator(self.comment)
+        return 'OptComment: {}'.format(truncated_comment.chars(10))
