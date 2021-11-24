2eab5f678d6b766d40eef2c905d450ec3b8d84e6
Fedor1533 <fgubanov1533@gmail.com>
2021-11-24 20:06:51 +0300

    Add init_surveys script.

---
+++ surveys/management/commands/init_surveys.py
@@ -0,0 +1,24 @@
+import csv
+from django.core.management import BaseCommand
+from django.utils import timezone
+
+from surveys.models import *
+
+
+class Command(BaseCommand):
+    help = "Create Survey objects."
+
+    def handle(self, *args, **options):
+        start_time = timezone.now()
+        for i in range(1, 9):
+            # self.stdout.write(f'Create survey {i}')
+            survey, create = Survey.objects.get_or_create(name=i)
+            if create:
+                survey.description = 'Survey' + str(i) + ' description'
+                survey.save()
+
+        self.stdout.write(f'End creating surveys')
+        end_time = timezone.now()
+        self.stdout.write(self.style.SUCCESS(f'Creating surveys took: {(end_time-start_time).total_seconds()} seconds.'))
+
+
